e20b72ce20db9712e7144a68671b2263ed8c59a2
Diego Toledo <Diego_toledo3010@hotmail.com>
2025-04-27 00:10:00 -0300

    Primeira versao sem refatoração

--- JogosDosDadosConsoleApp/Program.cs
+++ JogosDosDadosConsoleApp/Program.cs
@@ -1,10 +1,42 @@
 ﻿namespace JogosDosDadosConsoleApp
 {
+    /*Versão 1 - Estrutura básica e simulação de dados 
+        Requisitos:
+
+        Exibir um banner para o jogo de dados
+        Implementar a geração de números aleatórios para simular um dado (1-6)
+        Exibir o resultado do lançamento do dado
+        Permitir que o usuário pressione Enter para lançar o dado
+    */
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                Console.WriteLine("------------------------------------------------");
+                Console.WriteLine("Jogo dos Dados!");
+                Console.WriteLine("------------------------------------------------");
+
+                Console.WriteLine("Pressione enter para lançar o dado....");
+                Console.ReadLine();
+
+                Random geradordeNumeros = new Random();
+
+                int resultado = geradordeNumeros.Next(1, 7);
+
+                Console.WriteLine("------------------------------------------------");
+                Console.WriteLine($"O valor sorteado foi: {resultado}");
+                Console.WriteLine("------------------------------------------------");
+
+
+                Console.WriteLine("Deseja continuar? (S/N) ");
+                string opcaoContinuar = Console.ReadLine()!.ToUpper();
+
+                if (opcaoContinuar != "S")
+                    break;
+            }
+            
         }
     }
 }
